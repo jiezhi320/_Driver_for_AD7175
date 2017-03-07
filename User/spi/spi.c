f2c5f4be82ff90e6d77e06310bcf7ffc4d562e52
QUAN-PC\QUAN <shusheng1991@gmail.com>
2017-03-07 17:48:14 +0800

    17:48

--- User/spi/spi.c
+++ User/spi/spi.c
@@ -7,7 +7,7 @@ void spi_gpioConfig(void)
     GPIO_InitTypeDef SPI1_GPIO_InitStructure;
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
     SPI1_GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_7;
-    SPI1_GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;                            //∏¥”√Õ∆ÕÏ ‰≥ˆ
+    SPI1_GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;                            //Init IO port to AF output
     SPI1_GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
     SPI1_GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;                                  //MISO
